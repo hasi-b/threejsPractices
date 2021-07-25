5bce4fec722de6583f9febede3e6c2c2c14a40b6
Hasi-b <hasib.hasan9@gmail.com>
2021-07-25 15:11:24 +0600

    basic scene

--- 03.Basic Scene/script.js
+++ 03.Basic Scene/script.js
@@ -1,1 +1,31 @@
-console.log("Hello Three.js");
+
+
+//Scene
+const scene = new THREE.Scene();
+
+//Red Cube
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({color:'red'});
+const cube = new THREE.Mesh(geometry,material);
+scene.add(cube);
+//variable
+fov = 75;
+const sizes = {
+    width: 800,
+    height: 600
+};
+//camera
+
+const camera = new THREE.PerspectiveCamera(fov,sizes.width/sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+//renderer
+
+const canvas = document.querySelector('.webgl');
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width,sizes.height);
+
+renderer.render(scene,camera);
