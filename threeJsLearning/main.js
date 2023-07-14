2424ad5cb0e8ac57bbc511dcad2160b00273d54d
Hasi-b <hasib.hasan9@gmail.com>
2023-07-15 01:32:01 +0600

    added some css

--- threeJsLearning/main.js
+++ threeJsLearning/main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
+import "./style.css"
 import { Scene } from 'three';
+
 
 //Creating Scene
 const scene = new THREE.Scene();
@@ -14,18 +16,27 @@ const material = new THREE.MeshStandardMaterial({
 
 const mesh = new THREE.Mesh(geometry,material);
 scene.add(mesh);
+
+
+//Sizes
+const sizes = {
+  width : window.innerWidth,
+  height : window.innerHeight,
+}
+
+
 //Light
 const light = new THREE.PointLight(0xffffff,1,100);
 light.position.set(-10,10,10);
 scene.add(light);
 
 //Camera
-const camera = new THREE.PerspectiveCamera(45,800/600);
+const camera = new THREE.PerspectiveCamera(45,sizes.width/sizes.height);
 camera.position.z = 20;
 scene.add(camera);
 
 //Renderer
 const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({canvas: canvas});
-renderer.setSize(800,600);
+renderer.setSize(sizes.width,sizes.height);
 renderer.render(scene,camera);
