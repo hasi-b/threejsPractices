ce2e329599d597152d0e7149be072678fe66edbd
Hasi-b <hasib.hasan9@gmail.com>
2023-08-25 19:39:43 +0600

    using Orbit Controls

--- threeJsLearning/main.js
+++ threeJsLearning/main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import "./style.css"
 import { Scene } from 'three';
+import{OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
 
 
 //Creating Scene
@@ -27,13 +28,26 @@ const sizes = {
 
 //Light
 const light = new THREE.PointLight(0xffffff,1,100);
-light.position.set(10,10,10);
+light.position.set(10,20,10);
 scene.add(light);
 
 //Camera
 const camera = new THREE.PerspectiveCamera(45,sizes.width/sizes.height,0.1,100);
 camera.position.z = 20;
 scene.add(camera);
+
+
+//Controls
+
+const controls = new OrbitControls(camera,canvas);
+
+
+
+
+
+
+
+
 
 //Renderer
 const canvas = document.querySelector('.webgl');
@@ -47,6 +61,20 @@ window.addEventListener('resize',()=>{
   sizes.width= window.innerWidth;
   sizes.height = window.innerHeight;
   //Update Camera
-  camera.aspect = sizes.width,sizes.height;
+  
+  camera.aspect = sizes.width/sizes.height;
+  camera.updateProjectionMatrix();
   renderer.setSize(sizes.width,sizes.height);
 })
+
+const loop = ()=>{
+
+
+
+  
+  renderer.render(scene,camera);
+  window.requestAnimationFrame(loop);
+
+}
+loop();
+
