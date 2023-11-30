965102071cdba1fb47138d28cbc18e96d5f97a0f
Hasi-b <hasib.hasan9@gmail.com>
2023-11-30 09:31:19 +0600

    added shadows and imported new glb

--- PortfolioProjectThreeJs/Experience/World/Environment.js
+++ PortfolioProjectThreeJs/Experience/World/Environment.js
@@ -8,6 +8,8 @@ export default class Environment{
         this.scene = this.experience.scene;
         this.resources = this.experience.resources;
         this.setSunLight();
+        //this.setAreaLight();
+        //this.setSpotLight();
        
   
     }
@@ -16,11 +18,42 @@ export default class Environment{
         this.sunLight = new THREE.DirectionalLight("#ffffff",3);
         this.sunLight.castShadow = true;
         this.sunLight.shadow.camera.far = 20;
-        this.sunLight.shadow.mapSize.set(1024,1024);
+        this.sunLight.shadow.mapSize.set(2048,2048);
         this.sunLight.shadow.normalBias = 0.05;
-        this.sunLight.position.set(10,30,3);
+        this.sunLight.position.set(-5,10,10);
         this.scene.add(this.sunLight);
+
+        this.ambientLight = new THREE.AmbientLight("#ffffff",1);
+        this.scene.add(this.ambientLight);
         console.log("light added");
+   }
+   setAreaLight(){
+    this.width = 0.5;
+    this.height = 0.5;
+    this.intensity = 1;
+    this.rectLight = new THREE.RectAreaLight( 0xffffff, this.intensity, this.width, this.height );
+    this.rectLight.position.set( 5, 5, 0 );
+    this.rectLight.lookAt( 0, 0, 0 );
+    this.scene.add( this.rectLight );
+    this.rectAreaLightHelperGeometry = new THREE.PlaneGeometry(this.rectLight.width, this.rectLight.height);
+    this.rectAreaLightHelperMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00, side: THREE.DoubleSide, wireframe: true });
+    this.rectAreaLightHelperMesh = new THREE.Mesh(this.rectAreaLightHelperGeometry, this.rectAreaLightHelperMaterial);
+    this.rectAreaLightHelperMesh.position.copy(this.rectLight.position);
+    this.scene.add(this.rectAreaLightHelperMesh);
+   }
+
+   setSpotLight(){
+    this.spotLight = new THREE.SpotLight(0xff0000, 1, 100);
+    this.spotLight.position.set(0, 10, 0); 
+    this.spotLight.angle = Math.PI / 4; // Set the cone angle to 45 degrees (in radians)
+    this.spotLight.penumbra = 0.001;
+    
+    // Adjust position as needed
+    this.scene.add(this.spotLight);
+
+    // Visualize spotlight
+    this.spotLightHelper = new THREE.SpotLightHelper(this.spotLight);
+    this.scene.add(this.spotLightHelper);
    }
 
     resize(){
--- PortfolioProjectThreeJs/Experience/World/Room.js
+++ PortfolioProjectThreeJs/Experience/World/Room.js
@@ -19,10 +19,35 @@ export default class Room{
     }
 
    setModel(){
+       
+        this.actualRoom.children.forEach(child => {
+             child.castShadow = true;
+             child.receiveShadow = true;
+             console.log(child);
+             if(child instanceof THREE.Group){
+                child.children.forEach(groupChild=>{
+                    groupChild.castShadow = true;
+                    groupChild.receiveShadow = true;
+                });
+             }
+             if(child.name ==='Fish_Tank'){
+                child.material = new THREE.MeshPhysicalMaterial();
+                child.material.roughness =0.1;
+                child.material.color.set(0xffffff);
+                child.material.ior =3;
+                child.material.transmission =1;
+                child.material.opacity =1;
+             }
+             if(child.name ==='Pillow'){
+                child.castShadow = false;
+             }
+            
+        });
+
         this.scene.add(this.actualRoom);
         this.actualRoom.scale.set(0.09,0.09,0.09);
         this.actualRoom.position.set(0,-1,0);
-        this.actualRoom.rotation.y = Math.PI;
+        this.actualRoom.rotation.y = Math.PI*2;
    }
 
     resize(){
